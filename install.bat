6a0f2fcca14600215867c7825d91c2a7e8860ca4
Abe Pralle <abe.pralle@gmail.com>
2021-09-29 14:21:52 -1000

    [Windows] Added install.bat script

---
+++ install.bat
@@ -0,0 +1,20 @@
+@if exist "%HOMEDRIVE%%HOMEPATH%\AppData\Local\Morlock" goto CREATED_HOME
+@echo --------------------------------------------------------------------------------
+@echo Installing Morlock
+@echo --------------------------------------------------------------------------------
+@echo Creating home folder: %HOMEDRIVE%%HOMEPATH%\AppData\Local\Morlock
+@mkdir "%HOMEDRIVE%%HOMEPATH%\AppData\Local\Morlock"
+:CREATED_HOME
+
+@if exist "%HOMEDRIVE%%HOMEPATH%\AppData\Local\Morlock\build\abepralle\morlock" goto CREATED_BUILD
+@mkdir "%HOMEDRIVE%%HOMEPATH%\AppData\Local\Morlock\build\abepralle\morlock"
+:CREATED_BUILD
+
+@if exist "%HOMEDRIVE%%HOMEPATH%\AppData\Local\Morlock\build\abepralle\morlock\success.txt" goto DOWNLOADED_EXE
+@echo Downloading Morlock bootstrap executable...
+@curl -fsSL https://github.com/AbePralle/Morlock/releases/download/v0.0.3/morlock.exe -o "%HOMEDRIVE%%HOMEPATH%\AppData\Local\Morlock\build\abepralle\morlock\morlock.exe"
+@echo Success > "%HOMEDRIVE%%HOMEPATH%\AppData\Local\Morlock\build\abepralle\morlock\success.txt"
+:DOWNLOADED_EXE
+
+"%HOMEDRIVE%%HOMEPATH%\AppData\Local\Morlock\build\abepralle\morlock\morlock.exe" bootstrap
+
